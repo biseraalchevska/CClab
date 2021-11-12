13c381f7adaf0ee2a21c39530e89156ea7e7bbaf
bisera <ba1484@nyu.edu>
2021-11-12 15:30:45 +0800

    p5js sketch

--- js-and-dom/js/script.js
+++ js-and-dom/js/script.js
@@ -39,3 +39,52 @@ function say(what){
   }
 
 }
+
+
+let raindrops = [];
+num_raindrops = 50;
+function setup() {
+  createCanvas(600, 600);
+  for ( let i = 0;i<num_raindrops;i++){
+    raindrops.push(new Raindrop(random(width),random(height)));
+  }
+}
+
+function draw() {
+  background(0, 0, 255);
+  fill(0,127,0);
+  rect(0,300,width,height/2);
+  for(let i = 0;i<num_raindrops;i++){
+    raindrops[i].update();
+    raindrops[i].display();
+  }
+}
+
+class Raindrop{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.scale = random(1,3);
+    this.speedY = random(5,10);
+  }
+  update(){
+    this.y = this.y + this.speedY;
+    if (this.y >height/2){
+      this.y = 0;
+    }
+
+
+  }
+  display(){
+    push();
+    translate(this.x,this.y);
+    scale(this.scale);
+    fill(255);
+    noStroke();
+    ellipse(0,0,10,10);
+    triangle(-5,0,5,0,0,-10);
+    pop();
+
+
+  }
+}
